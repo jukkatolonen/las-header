cc56a52c4f3cadd873e2ae64ae6cd9ddc7f5faea
jukkatolonen <jukka@tripoint.fi>
2020-02-15 17:34:38 +0100

    save me

--- src/lasHeader.js
+++ src/lasHeader.js
@@ -7,28 +7,28 @@ const {
     fileObjToBuffer,
  } = require('./utils/helper');
 
-const assertInput = (options) => {    
+const assertOptions = (options) => {    
     if (!options.input) {
         throw new Error('las-header: Input required');   
     }  
 };
 
 const readLocalFile = (options = {}) => { 
-    assertInput(options);
+    assertOptions(options);
     const input = options.input;
     const buffer = bufferFromLocalFile({input});
     return parseHeaders({buffer}); 
 };
 
 const readUrl = async (options = {}) => { 
-    assertInput(options);
+    assertOptions(options);
     const input = options.input;
     const {buffer, arrayBuffer} = await bufferFromUrl({input});
     return parseHeaders({buffer, arrayBuffer});   
 };
 
 const readFileObject = async (options = {}) => {
-    assertInput(options);
+    assertOptions(options);
     const input = options.input;
     const {buffer, arrayBuffer} = await fileObjToBuffer({input});
     return parseHeaders({buffer, arrayBuffer}); 
